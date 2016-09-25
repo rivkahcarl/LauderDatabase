151bd14b71d69d27ce38bc5920ce165c9458dc97
Benny <benjaminhimley85@gmail.com>
2016-09-24 21:31:24 -0700

    Add student contact

--- student_database/students/forms.py
+++ student_database/students/forms.py
@@ -1,10 +1,18 @@
 from django import forms
 from django.contrib.auth.models import User
 
+from students.models import StaffContact
+
 
 class RegistrationForm(forms.ModelForm):
-    """ TODO-Benny: add check in clean() for already existing username """
     class Meta:
         model = User
         fields = ('username', 'password')
         widgets = {'password': forms.PasswordInput()}
+
+
+class StaffContactForm(forms.ModelForm):
+    class Meta:
+        model = StaffContact
+        fields = ('note', 'conversation_date_time', 'student')
+        widgets = {"student": forms.HiddenInput()}
--- student_database/students/views.py
+++ student_database/students/views.py
@@ -2,7 +2,8 @@ from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
 from django.urls import reverse_lazy
 
-from students.models import Student
+from students.models import Student, StaffContact
+from students.forms import StaffContactForm
 
 
 STUDENT_FIELDS = ['first_name', 'last_name', 'address',
@@ -24,3 +25,18 @@ class StudentUpdate(UpdateView):
     model = Student
     fields = STUDENT_FIELDS
     template_name_suffix = '_update_form'
+
+
+class StaffContactCreate(CreateView):
+    model = StaffContact
+    form_class = StaffContactForm
+    success_url = reverse_lazy('students:students-list')
+
+    def get_form(self, form_class=None):
+        if form_class is None:
+            form_class = self.get_form_class()
+        initials = {
+            "student": self.kwargs['student_id']
+        }
+        form = form_class(initial=initials, data=self.request.POST or None)
+        return form
